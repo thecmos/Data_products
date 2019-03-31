9884d2a5083284e3e42a1c441cb04aba89371ab5
thecmos <jloresb@hotmail.com>
2019-03-30 23:55:56 -0500

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,4 +1,4 @@
-
+library(shiny)
 shinyUI(fluidPage(
   fluidRow(
     column(12,offset=4,
@@ -12,29 +12,34 @@ shinyUI(fluidPage(
     )
   ),
   fluidRow(
-    column(2,offset=2,
+    column(3,
            checkboxInput("show_xlab", "Show/Hide X Axis Label", value = TRUE)
     ),
-    column(2,
+    column(3,
            checkboxInput("show_ylab", "Show/Hide Y Axis Label", value = TRUE)
     ),
-    column(2,
+    column(3,
            checkboxInput("show_title", "Show/Hide Title",value=TRUE)
     ),
-    column(2,
+    column(3,
            checkboxInput("show_labels", "Show/Hide Labels",value=TRUE)
     )
     
-  ),
-  hr(),
+  )
+  
+  ,
+  
   fluidRow(
-    column(12,div(style="height:10px"),plotOutput('plot1') )
+    column(12,div(style="height:10px"),plotOutput('plot1',height="400px") )
     
   )
+  
   ,
+  
+  
   fluidRow(
     
-    column(12,align="center",offsett=5, div(style="height:10px"), h5("Count of cities:"),textOutput('text1') )
+    column(12,align="center", h5("Count of cities:"),textOutput('text1') )
     
   )
   
@@ -44,6 +49,6 @@ shinyUI(fluidPage(
     
   )
   
+  
 )
 )
-
